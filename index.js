0233fb99dbeadb5876cfd07d9ca31472fa1cde6a
bhavesh <bhavesh@ktds.co>
2018-06-18 21:29:35 +0530

    local and remote db

--- index.js
+++ index.js
@@ -1,16 +1,12 @@
-import PouchDB from 'pouchdb-core'
-import mapreduce from 'pouchdb-mapreduce'
 import SQLite from 'react-native-sqlite-2'
-import replication from './lib/replication'
 import remote from './lib/remote-adapter'
 import sql from './lib/sqlite'
 
 const SQLiteAdapter = sql(SQLite)
 
-PouchDB
-  .plugin(SQLiteAdapter)
-  .plugin(remote)
-  .plugin(mapreduce)
-  .plugin(replication)
+function addAdapter (PouchDB) {
+  remote(PouchDB)
+  SQLiteAdapter(PouchDB)
+}
 
-export default PouchDB
+export default addAdapter
