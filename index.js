8c4117d566d7517a485dcfe20fd1c1fcd8df24fd
bhavesh <bhav.khandelwal@gmail.com>
2018-08-24 18:47:57 +0530

    minor changes

--- index.js
+++ index.js
@@ -1,19 +1,12 @@
-'use strict';
-
-import PouchDB from 'pouchdb-core';
-import mapreduce from 'pouchdb-mapreduce';
 import SQLite from 'react-native-sqlite-2';
 import remote from './lib/remote-adapter';
-import replication from './lib/replication';
 import sql from './lib/sqlite';
 
-function addAdapter(PDB) {
-  const SQLiteAdapter = sql(SQLite);
-  remote(PDB);
-  SQLiteAdapter(PDB);
-  replication(PDB);
+const SQLiteAdapter = sql(SQLite);
+
+function addAdapter(PouchDB) {
+  remote(PouchDB);
+  SQLiteAdapter(PouchDB);
 }
 
-PouchDB.plugin(addAdapter).plugin(mapreduce);
-
-export default PouchDB;
+export default addAdapter;
